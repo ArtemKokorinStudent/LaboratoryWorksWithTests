d333da7190b0d17c610c84105ecfba74105acc32
simplydesigner <simplydesigner@ya.ru>
2016-03-14 11:47:26 +0300

    refactored

--- build/tests/CMakeFiles/github_designerror_cpmcatch_origin_master.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/github_designerror_cpmcatch_origin_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
