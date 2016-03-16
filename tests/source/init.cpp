4c6598788061f5e9d2ac4216ba77d01466c2ddb3
simplydesigner <simplydesigner@ya.ru>
2016-03-16 10:47:22 +0300

    added fill test

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,7 +1,7 @@
 #include <Matrix.hpp>
 #include <catch.hpp>
 
-SCENARIO("Matrix", "[init]") {
+SCENARIO("Matrix init", "[init]") {
 
 	GIVEN("The number of rows and columns") {
 
@@ -19,4 +19,21 @@ SCENARIO("Matrix", "[init]") {
 			}
 		}
 	}
+}
+
+SCENARIO("Matrix >>", "[fill]") {
+	Matrix A = Matrix(2, 2);
+	REQUIRE( A.fill("A2x2.txt") );
+}
+
+SCENARIO("Matrix +", "[addition]") {
+	Matrix A = Matrix(2, 2);
+	A.fill("A2x2.txt");
+	Matrix B = Matrix(2, 2);
+	B.fill("B2x2.txt");
+	Matrix expected = Matrix(2, 2);
+	expected.fill("A+B2x2.txt");
+
+	Matrix result = A + B;
+	REQUIRE(result == expected);
 }
