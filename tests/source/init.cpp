454534d17d0898367d846adb5bfb2bf2338342eb
TrigDevelopment <xx.artem-kokorin@mail.ru>
2016-03-30 09:24:45 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,6 +2,13 @@
 #include <catch.hpp>
 #include <fstream>
 
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
 /*SCENARIO("Matrix init", "[init]") {
 
 	GIVEN("The number of rows and columns") {
