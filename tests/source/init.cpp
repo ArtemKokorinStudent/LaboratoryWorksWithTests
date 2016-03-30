a64b552408c9f38007f53ffe480c2cba236d90ee
TrigDevelopment <xx.artem-kokorin@mail.ru>
2016-03-30 09:26:38 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 1 );
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
