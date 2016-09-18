d6ee0bc4c7c2a2e6427d5a563f2f401643f70b7d
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-09-18 19:28:14 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,129 +1,46 @@
 #include <catch.hpp>
 
-#include <Matrix.hpp>
-#include <fstream>
+#include "stack.hpp"
 
-SCENARIO("Matrix init", "[init]") {
-	GIVEN("The number of lines and columns") {
-		auto lines = 36;
-		auto columns = 39;
+TEST_CASE("Stack can be instantiated by various types", "[instantiation]") {
+	REQUIRE_NOTHROW(stack<int> st1);
+	REQUIRE_NOTHROW(stack<double> st1);
+	REQUIRE_NOTHROW(stack<char> st1);
+	REQUIRE_NOTHROW(stack<int*> st1);
+	REQUIRE_NOTHROW(stack<stack<int>> st1);
+}
+TEST_CASE("Push, pop", "[push_pop]") {
+	stack<int> st;
+	st.push(1);
+	st.push(2);
+	REQUIRE(st.pop() == 2);
+	REQUIRE(st.pop() == 1);
+}
+TEST_CASE("count", "[count]") {
+	stack<int> st;
+	REQUIRE(st.count() == 0);
+	st.push(1);
+	REQUIRE(st.count() == 1);
+	st.push(2);
+	REQUIRE(st.count() == 2);
+	st.pop();
+	REQUIRE(st.count() == 1);
+	st.pop();
+	REQUIRE(st.count() == 0);
+}
+TEST_CASE("Copy constructor, =", "[copy_ctr, =]") {
+	stack<int> st1;
+	st1.push(1);
+	st1.push(2);
+	REQUIRE_NOTHROW(stack<int> st2 = st1);
+	REQUIRE_NOTHROW(stack<int> st2;
+		st2 = st1);
 
-		WHEN("Create instansce of Matrix") {
-			Matrix<int> matrix(lines, columns);
-			THEN("The number of lines and columns must be preserved") {
-				REQUIRE(matrix.GetNumberOfLines() == lines);
-				REQUIRE(matrix.GetNumberOfColumns() == columns);
-			}
-		}
-	}
+	stack<int> st3 = st1;
+	stack<int> st4 = st1;
+	REQUIRE(st1.pop() == 2);
+	REQUIRE(st3.pop() == 2);
+	REQUIRE(st3.pop() == 1);
+	REQUIRE(st4.pop() == 2);
+	REQUIRE(st4.pop() == 1);
 }
-
-SCENARIO("Matrix InitFromFile", "[fill]") {
-	Matrix<int> C(3, 1);
-	C.InitFromFile("C3x1.txt");
-	REQUIRE( C[0][0] == 1 );
-	REQUIRE( C[1][0] == 2 );
-	REQUIRE( C[2][0] == 0 );
-}
-SCENARIO("Matrix Output", "[output]") {
-	Matrix<int> A(5, 4);
-	A.InitFromRandom();
-	std::cout << A;
-}
-SCENARIO("Matrix =", "assignment") {
-	Matrix<int> A(3, 3);
-	A.InitFromFile("A3x3.txt");
-	Matrix<int> B(4, 2);
-	B = A;
-	for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 3; j++){
-			REQUIRE( A[i][j] == B[i][j] );
-		}
-	}
-}
-SCENARIO("Matrix +", "[addition]") {
-	Matrix<int> A(3, 3);
-	A.InitFromFile("A3x3.txt");
-	Matrix<int> B(3, 3);
-	B.InitFromFile("B3x3.txt");
-	Matrix<int> expected = Matrix<int>(3, 3);
-	expected.InitFromFile("(A3x3)+(B3x3).txt");
-
-	Matrix<int> result = A + B;
-	for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 3; j++){
-			REQUIRE(result[i][j] == expected[i][j]);
-		}
-	}
-}
-SCENARIO("Matrix *", "[multiplication]") {
-	Matrix<int> A = Matrix<int>(3, 3);
-	A.InitFromFile("A3x3.txt");
-	Matrix<int> C = Matrix<int>(3, 1);
-	C.InitFromFile("C3x1.txt");
-	Matrix<int> expected = Matrix<int>(3, 1);
-	expected.InitFromFile("(A3x3)(C3x1).txt");
-
-	Matrix<int> result = A * C;
-	for (int i = 0; i < 3; i++){
-		REQUIRE(result[i][0] == expected[i][0]);
-	}
-}
-SCENARIO("Matrix ==", "[comparsion]"){
-	Matrix<int> A = Matrix<int>(3, 3);
-	A.InitFromFile("A3x3.txt");
-	Matrix<int> C = Matrix<int>(3, 1);
-	C.InitFromFile("C3x1.txt");
-	
-	REQUIRE(A != C);
-	REQUIRE(A == A);
-}
-SCENARIO("Bad Matrix *", "[badmultiplication]") {
-	Matrix<int> A = Matrix<int>(3, 3);
-	A.InitFromFile("A3x3.txt");
-	Matrix<int> C = Matrix<int>(3, 1);
-	C.InitFromFile("C3x1.txt");
-	bool exception_flag = false;
-	try{
-		C * A;
-	} catch (...){
-		exception_flag = true;
-	}
-	REQUIRE(exception_flag);
-}
-SCENARIO("Bad Matrix +", "[badplus") {
-	Matrix<int> A = Matrix<int>(3, 3);
-	A.InitFromFile("A3x3.txt");
-	Matrix<int> C = Matrix<int>(3, 1);
-	C.InitFromFile("C3x1.txt");
-	bool exception_flag = false;
-	try{
-		A + C;
-	} catch (...){
-		exception_flag = true;
-	}
-	REQUIRE(exception_flag);
-}
-SCENARIO("Bad matrix InitFromFile", "[fill]"){
-	std::cout << "Bad >>";
-	Matrix<int> C(4, 5);
-	bool exception_flag = false;
-	try{
-		C.InitFromFile("A8x3.txt");
-	} catch (...){
-		exception_flag = true;
-	}
-	REQUIRE(exception_flag);
-}
-
-SCENARIO("Bad []", "[badindex]") {
-	Matrix<int> C(3, 1);
-	bool exception_flag = false;
-	try{
-		C[1000];
-	} catch (...){
-		exception_flag = true;
-	}
-	REQUIRE(exception_flag);
-}
-
